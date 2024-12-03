5a8649c99fbb4b861ef9588b07d40166cfe4c24d
tuanqwerty <tuanthai902@gmail.com>
2024-12-03 15:41:43 +0700

    update

--- tailwincss/tailwind.config.js
+++ tailwincss/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/html/*.html'],
+  content: ['./src/html/*.{html,php,js}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: theme => ({
+        URL: "url('https://source.unsplash.com/random')",
+      }),
+    },
   },
   plugins: [],
 }
