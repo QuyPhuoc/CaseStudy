451a0869598fdc3fa845413e12db3d33386beb44
tuanqwerty <tuanthai902@gmail.com>
2024-12-10 15:32:25 +0700

    update

--- tailwincss/src/php/crudPost.php
+++ tailwincss/src/php/crudPost.php
@@ -6,7 +6,11 @@ function getAllMotels() {
     $sql = "SELECT * FROM Motel";
     $res = $conn->query($sql);
     if($res === TRUE){
-        return $res;
+        $result = array();
+        while($row = $res->fetch_assoc()){
+            $result[] = $row;
+        }
+        return json_encode($result);
     }else{
         echo "Loi: ".$sql."<br>".$conn->error;
     }
@@ -17,16 +21,20 @@ function updateMotel($id,$title, $description, $price, $area, $address, $latlng,
     $conn = getDBConnection();
     $sql = "UPDATE Motel SET title = '$title', description = '$description', price = '$price', area = '$area', address = '$address', latlng = '$latlng', phone = '$phone', utilites = '$utilities'  WHERE id = '$id'";
     $res = $conn->query($sql);
-    if($res->num_rows() > 0){
-       
-    } 
+    if($res === TRUE){
+        echo "Cap nhat thanh cong";
+    }else{
+        echo "Loi: ".$sql."<br>".$conn->error;
+    }
 }
 function deleteMotel($id){
     $conn = getDBConnection();
     $sql = "DELETE FROM Motel WHERE id = '$id'";
     $res = $conn->query($sql);
-    if($res->num_rows() > 0){
+    if($res === TRUE){
         echo "Xoa thanh cong";
-    } 
+    }else{
+        echo "Loi: ".$sql."<br>".$conn->error;
+    }
 }
 ?>
